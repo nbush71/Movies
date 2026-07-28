4e763c40c775b2e9c9176d80296dbd8a845acae6
Nadine Bush <32846095+nbush71@users.noreply.github.com>
2026-07-28 15:56:50 -0500

    poster js

--- poster.js
+++ poster.js
@@ -1,37 +1,29 @@
-// user.js code goes in here
-const postListEl = document.querySelector('.post-list');
+const movieListEl = document.querySelector(".movie-list");
 const id = localStorage.getItem("id")
 
 async function onSearchChange(event) {
     const id = event.target.value;
-    renderPosts(id)
+    renderMovie(id)
 }
 
-async function renderPosts(id) {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId || id}`);
-    const postsData = await response.json();
-    postListEl.innerHTML = postsData.map(post => postHTML(post)).join('');
+async function renderMovie(id) {
+    const response = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=bd39f942&s`);
+    const movieData = await response.json();
+    movieListEl.innerHTML = movieData.map(movie => movieHTML(id)).join('');
 }
 
-function postsHTML(post) {
+function movieHTML(movie) {
     return `
-        <div class="post">
-            <div class="post__title">
-                ${post.title}
-            </div>
-            <p class="post__body">
-                ${post.body}
-            </p>
-        </div>
         <div class="movie-card">
             <div class="movie__card--container">
-                <h3>Movie Search</h3>
-                <p><b>Movie Title:</b>${post.title}</p>
-                <p><b>Year:</b>${post.year}</p>
-                <p><b>ID:</b>${post.id}</p>
-                <p><b>Poster:</b>${post.poster}</p>
+                <p><b>Movie Title:</b>${movie.title}</p>
+                <p><b>Year:</b>${movie.year}</p>
+                <p><b>ID:</b>${movie.id}</p>
+                <p><b>Poster:</b>${movie.poster}</p>
             </div>
         </div>`
 }
 
-renderPosts(id);
+renderMovie(id);
+
+console.log(id);
