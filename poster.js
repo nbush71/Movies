2854f97c8339abc1a0f2e580859f41738df9f33a
Nadine Bush <32846095+nbush71@users.noreply.github.com>
2026-07-29 11:45:19 -0500

    JS fixes from mentor

--- poster.js
+++ poster.js
@@ -1,29 +1,56 @@
 const movieListEl = document.querySelector(".movie-list");
-const id = localStorage.getItem("id")
 
 async function onSearchChange(event) {
-    const id = event.target.value;
-    renderMovie(id)
+    const title = event.target.value.trim();
+
+    if (!title) {
+        movieListEl.innerHTML = "<p>Please type a movie title.</p>";
+        return;
+    }
+
+    renderMovies(title);
 }
 
-async function renderMovie(id) {
-    const response = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=bd39f942&s`);
-    const movieData = await response.json();
-    movieListEl.innerHTML = movieData.map(movie => movieHTML(id)).join('');
+async function renderMovies(title) {
+    movieListEl.innerHTML = "<p>Loading movies...</p>";
+
+    try {
+        const response = await fetch(
+            `https://www.omdbapi.com/?apikey=bd39f942&s=${encodeURIComponent(title)}`
+        );
+
+        if (!response.ok) {
+            throw new Error("The movie request failed.");
+        }
+
+        const movieData = await response.json();
+
+        if (movieData.Response === "False") {
+            movieListEl.innerHTML = `<p>${movieData.Error}</p>`;
+            return;
+        }
+
+        movieListEl.innerHTML = movieData.Search.map(movieHTML).join("");
+    } catch (error) {
+        movieListEl.innerHTML = "<p>Sorry, movies could not be loaded.</p>";
+        console.error(error);
+    }
 }
 
 function movieHTML(movie) {
+    const poster = movie.Poster === "N/A"
+        ? "<p>No poster available</p>"
+        : `<img src="${movie.Poster}" alt="${movie.Title} poster">`;
+
     return `
         <div class="movie-card">
             <div class="movie__card--container">
-                <p><b>Movie Title:</b>${movie.title}</p>
-                <p><b>Year:</b>${movie.year}</p>
-                <p><b>ID:</b>${movie.id}</p>
-                <p><b>Poster:</b>${movie.poster}</p>
+                <p><b>Movie Title:</b> ${movie.Title}</p>
+                <p><b>Year:</b> ${movie.Year}</p>
+                <p><b>ID:</b> ${movie.imdbID}</p>
+                ${poster}
             </div>
-        </div>`
+        </div>`;
 }
 
-renderMovie(id);
-
-console.log(id);
+renderMovies("Fast");
