67656f8fe8e53c4d22fc1b19d00ff951db3b2ae9
Nadine Bush <32846095+nbush71@users.noreply.github.com>
2026-07-30 16:34:22 -0500

    updates save

--- poster.js
+++ poster.js
@@ -54,11 +54,3 @@ function movieHTML(movie) {
 }
 
 renderMovies("Fast");
-
-function openMenu() {
-    document.body.classList.add("menu--open");
-}
-
-function closeMenu() {
-    document.body.classList.remove("menu--open");
-}
