62250fa26d74d3a986c7e1b26bf0b5475ec06690
Nadine Bush <32846095+nbush71@users.noreply.github.com>
2026-08-03 19:11:56 -0500

    Refactor sortMenu event listener initialization

--- sort.js
+++ sort.js
@@ -1,15 +1,15 @@
-if (sortMenu) {
-  sortMenu.addEventListener("change", (event) => {
-    const sortedMovies = [...movies];
+const sortMenu = document.querySelector("#sortMenu");
 
-    if (event.target.value === "az") {
-      sortedMovies.sort((a, b) => a.Title.localeCompare(b.Title));
-    }
+sortMenu.addEventListener("change", (event) => {
+  const sortedMovies = [...movies];
 
-    if (event.target.value === "za") {
-      sortedMovies.sort((a, b) => b.Title.localeCompare(a.Title));
-    }
+  if (event.target.value === "az") {
+    sortedMovies.sort((a, b) => a.Title.localeCompare(b.Title));
+  }
 
-    displayMovies(sortedMovies);
-  });
-}
+  if (event.target.value === "za") {
+    sortedMovies.sort((a, b) => b.Title.localeCompare(a.Title));
+  }
+
+  displayMovies(sortedMovies);
+});
