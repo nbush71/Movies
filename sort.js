93da0dab28c6795a0a632c3b4c20d96646ff0919
Nadine Bush <32846095+nbush71@users.noreply.github.com>
2026-08-03 19:02:59 -0500

    sort fix

--- sort.js
+++ sort.js
@@ -1,21 +1,18 @@
-// Sort functionality for the movie posters page
-// A-Z
-// Z-A
-// Newest
-// Oldest
-
 const sortMenu = document.querySelector("#sortMenu");
 
-sortMenu.addEventListener("change", (event) => {
-  const sortedMovies = [...movies];
+if (sortMenu) {
+  sortMenu.addEventListener("change", (event) => {
+    const sortedMovies = [...movies];
 
-  if (event.target.value === "az") {
-    sortedMovies.sort((a, b) => a.Title.localeCompare(b.Title));
-  }
+    if (event.target.value === "az") {
+      sortedMovies.sort((a, b) => a.Title.localeCompare(b.Title));
+    }
 
-  if (event.target.value === "za") {
-    sortedMovies.sort((a, b) => b.Title.localeCompare(a.Title));
-  }
+    if (event.target.value === "za") {
+      sortedMovies.sort((a, b) => b.Title.localeCompare(a.Title));
+    }
 
-  displayMovies(sortedMovies);
-});
+    displayMovies(sortedMovies);
+  });
+if (sortMenu) {
+}
