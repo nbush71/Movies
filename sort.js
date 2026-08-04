daa54112a87b9229a9442b87e1be5ad8a7f40190
Nadine Bush <32846095+nbush71@users.noreply.github.com>
2026-08-03 19:19:07 -0500

    sort fix

--- sort.js
+++ sort.js
@@ -1,15 +1,17 @@
 const sortMenu = document.querySelector("#sortMenu");
 
-sortMenu.addEventListener("change", (event) => {
-  const sortedMovies = [...movies];
+if (sortMenu) {
+  sortMenu.addEventListener("change", (event) => {
+    const sortedMovies = [...movies];
 
-  if (event.target.value === "az") {
-    sortedMovies.sort((a, b) => a.Title.localeCompare(b.Title));
-  }
+    if (event.target.value === "az") {
+      sortedMovies.sort((a, b) => a.Title.localeCompare(b.Title));
+    }
 
-  if (event.target.value === "za") {
-    sortedMovies.sort((a, b) => b.Title.localeCompare(a.Title));
-  }
+    if (event.target.value === "za") {
+      sortedMovies.sort((a, b) => b.Title.localeCompare(a.Title));
+    }
 
-  displayMovies(sortedMovies);
-});
+    displayMovies(sortedMovies);
+  });
+}
